b8e9b61a83580e365146180fbbf2ff790cbe0faa
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-07 21:08:16 -0500

    final homework

--- week1/day2/homeworkPython101.py
+++ week1/day2/homeworkPython101.py
@@ -2,15 +2,15 @@
 # Write a function that will flip a coin and print result
 # should be a 50/50 chance of heads or tails
 
-# import random
-# sideOfCoin = random.randint(0,2) #making a random choice
-# def flipACoin (): #creating a function
-#     if sideOfCoin < 1:
-#         print('You flipped a coin!')
-#         print('It is heads!')
-#     if sideOfCoin > 1:
-#         print('You flipped a coin!')
-#         print('It is tails')
+import random
+sideOfCoin = random.randint(0,2) #making a random choice
+def flipACoin (): #creating a function
+    if sideOfCoin < 1:
+        print('You flipped a coin!')
+        print('It is heads!')
+    if sideOfCoin > 1:
+        print('You flipped a coin!')
+        print('It is tails')
 
 # flipACoin() #calling the function
 
@@ -18,13 +18,13 @@
 #Even Odd
 #write a function when given a number as an input, it tells the user if it is even or odd
 
-# num = int(input('Enter a number: '))
+num = int(input('Enter a number: '))
 
-# def evenOrOdd (num) :
-#     if (num % 2) == 0:
-#         print("It's Even!")
-#     else:
-#         print("It's Odd!")
+def evenOrOdd (num) :
+    if (num % 2) == 0:
+        print("It's Even!")
+    else:
+        print("It's Odd!")
 
 # evenOrOdd(num)
 
