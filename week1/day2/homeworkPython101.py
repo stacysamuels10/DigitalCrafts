e9e9e85842c2303f3d155a998f55ef75fd26ac15
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-07 21:07:50 -0500

    Dice Roller

--- week1/day2/homeworkPython101.py
+++ week1/day2/homeworkPython101.py
@@ -16,13 +16,35 @@
 
 
 #Even Odd
+#write a function when given a number as an input, it tells the user if it is even or odd
 
-num = int(input('Enter a number: '))
+# num = int(input('Enter a number: '))
 
-def evenOrOdd (num) :
-    if (num % 2) == 0:
-        print("It's Even!")
-    else:
-        print("It's Odd!")
+# def evenOrOdd (num) :
+#     if (num % 2) == 0:
+#         print("It's Even!")
+#     else:
+#         print("It's Odd!")
 
-evenOrOdd(num)
+# evenOrOdd(num)
+
+# dice roller
+# write a function that take two numbers as arguments #int(input())
+#write a script that tells the user we are rolling a dice and they need to decide number of sides
+#chooses a random integer within the amount the user gave
+
+import random
+from time import sleep
+
+print("Let's roll a dice!")
+
+pickADice = int(input('How many sides should it have? (2-20): '))
+
+diceSides = random.randint(2,pickADice)
+
+def rollTheDice (diceSides):
+    print("It's rolling...")
+    sleep(2)
+    print("You rolled " + str(diceSides) + "!")
+
+rollTheDice(diceSides)
