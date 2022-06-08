25bf35a9b3d61a3b24b2673b2e996574f2aa1f18
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-07 20:55:13 -0500

    Even Odd

--- week1/day2/homeworkPython101.py
+++ week1/day2/homeworkPython101.py
@@ -2,14 +2,27 @@
 # Write a function that will flip a coin and print result
 # should be a 50/50 chance of heads or tails
 
-import random
-sideOfCoin = random.randint(0,2) #making a random choice
-def flipACoin (): #creating a function
-    if sideOfCoin < 1:
-        print('You flipped a coin!')
-        print('It is heads!')
-    if sideOfCoin > 1:
-        print('You flipped a coin!')
-        print('It is tails')
+# import random
+# sideOfCoin = random.randint(0,2) #making a random choice
+# def flipACoin (): #creating a function
+#     if sideOfCoin < 1:
+#         print('You flipped a coin!')
+#         print('It is heads!')
+#     if sideOfCoin > 1:
+#         print('You flipped a coin!')
+#         print('It is tails')
 
-flipACoin() #calling the function
+# flipACoin() #calling the function
+
+
+#Even Odd
+
+num = int(input('Enter a number: '))
+
+def evenOrOdd (num) :
+    if (num % 2) == 0:
+        print("It's Even!")
+    else:
+        print("It's Odd!")
+
+evenOrOdd(num)
