10e904551218acd1643af8d7d5214e824d70061e
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-08 14:30:15 -0500

    Update day3TicTacToe.py

--- week1/day3/day3TicTacToe.py
+++ week1/day3/day3TicTacToe.py
@@ -1,14 +1,14 @@
 SIZE = 3
-empty = " "
+empty = " |"
 board = []
 
 for y in range(SIZE):
     board.append([])
     for x in range(SIZE):
-        board[y].append("[%s]" % (empty))
+        board[y].append("%s" % (empty))
 
 for row in board:
     for column in row:
-        print("%s  " % column, end="")
+        print("%s_" % column, end=" ")
     print("\n")
 
