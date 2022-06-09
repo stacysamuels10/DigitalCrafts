c5c838ceb827c18dfad326c92d452ea7d02fe76b
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-08 20:19:19 -0500

    Update day3TwoNumberSum.py

---
+++ week1/day3/day3TwoNumberSum.py
@@ -0,0 +1,16 @@
+#Three Number Sum
+#no repeating data, give pos and neg numbers, go through this lists
+#find triplets that equal given number
+
+nums = [2,7,11,15]
+target = 9
+
+def twoSum (nums):
+    for i in nums:
+        for j in nums:
+            if i + j == target:
+                return [nums.index(i), nums.index(j)]
+
+newNumber = twoSum(nums)
+
+print(newNumber)
