860f505ed6c5146b454f8e88045826195ade7e94
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-13 11:57:28 -0500

    fitting the function in the class

--- week2/day1/activityUserAddress.py
+++ week2/day1/activityUserAddress.py
@@ -3,7 +3,8 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
-    
+    def createNewAddress (self, stacyAddress):
+        stacyUser.address.append(stacyAddress)
 
 
 class Address:
@@ -15,6 +16,8 @@ class Address:
 
 stacyUser = User("Stacy", "Samuels")
 stacyAddress = Address("NoWhereLand", "Baton Rouge", "LA", "11111")
-stacyUser.address.append(stacyAddress)
 
-print(stacyUser.address[0].street)
+
+
+stacyUser.createNewAddress(stacyAddress)
+print(stacyUser.address[0].zip_code)
