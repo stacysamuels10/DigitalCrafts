b3d1380e2863153f924cd4976e8e7fea0a2284f4
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-13 10:52:50 -0500

    Update example1.py

--- week2/day1/example1.py
+++ week2/day1/example1.py
@@ -14,12 +14,25 @@ class Room:
         self.flooring = flooring
         self.name = name
         self.contents = contents
+    def nameOfRoom(self):
+        print(self.name)
+    def lowerCaseRoomName(self):
+        self.name = self.name.lower()
 
 room1 = Room("12x12x15", 2, 2, "tile", "The Streaming Room", ["Mic", "Mouse", "PC"])
 #print(room1.dimensions)
 
-if room1.name == "The Streaming Room":
-    print("Yup, this is where we stream")
+# if room1.name == "The Streaming Room":
+#     print("Yup, this is where we stream")
 
-for content in room1.contents:
-    print(content)
+# for content in room1.contents:  #blank.blank is called dot notation
+#     print(content)
+
+
+#instance of a room
+blakesRoom = Room("5x6x6", 0, 1, "stone", "PRIson cell", ["Empty Pot", "Pile of hay", "chains", "rat"])
+# blakesRoom.nameOfRoom()
+
+blakesRoom.nameOfRoom()
+blakesRoom.lowerCaseRoomName()
+blakesRoom.nameOfRoom()
