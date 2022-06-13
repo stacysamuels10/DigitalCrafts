a6da819857d48abe8f099c1f0048545ae33bbafa
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-13 09:53:13 -0500

    morning notes

--- week2/day1/classes.py
+++ week2/day1/classes.py
@@ -1,2 +1,17 @@
 class User:
-    pass
+    # name
+    # height
+    # weight
+    # siblings
+    # greeting
+    def __init__(self, name, height):
+        self.name = name
+        self.height = height
+        self.weight = "none of your business"
+        self.siblings = "Joe"
+        self.greeting = "Hey guys"
+
+rahmin = User("rahmin", "5.9" )
+Olivia = User("Olivia", "5.8")
+print(rahmin.name, rahmin.height)
+print(Olivia.name, Olivia.height)
