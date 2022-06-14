c178b1c5211911d2e85618d3187085b26b403a9f
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-14 11:25:12 -0500

    morning practice

--- week2/day2/superClassPractice.py
+++ week2/day2/superClassPractice.py
@@ -8,18 +8,21 @@ class Parent:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+    def schoolDay(self):
+        print("How was your day at school?")
 
 class Child(Parent):
-    def __init(self, first_name, last_name, age, sibling_order):
+    def __init__(self, first_name, last_name, age, sibling_order):
         super().__init__(first_name, last_name, age)
         self.sibling_order = sibling_order
+    def answerSchoolDay(self):
+        answer = input("What is your answer?\n")
+        print(answer)
 
 Lisa = Parent("Lisa", "Samuels", "55")
 Sheldon = Parent("Sheldon", "Samuels", "53")
 Jason = Child("Jason", "Samuels", "32", 1)
 Stacy = Child("Stacy", "Samuels", "30", 2)
 
-print(lisa.first_name)
-print(sheldon.last_name)
-print(jason.age)
-orint(stacy.sibling_order)
+Sheldon.schoolDay()
+Stacy.answerSchoolDay()
