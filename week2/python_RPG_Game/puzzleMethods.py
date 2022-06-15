95ce50b411506fca8d0bf29f45f2672c225a6617
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-15 15:59:49 -0500

    Update puzzleMethods.py

--- week2/python_RPG_Game/puzzleMethods.py
+++ week2/python_RPG_Game/puzzleMethods.py
@@ -1,9 +1,10 @@
 import OptionsScripts
+whatNext = ""
+backpackChoice = 0
+menuChoice = 0
 
 def puzzle1 ():
     listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door", "4. Quit the game"]
-    whatNext = ""
-    menuChoice = 0
     print("\n")
     for items in listOfMenuOptions:
         print(items)
@@ -12,72 +13,72 @@ def puzzle1 ():
     if whatNext.isnumeric() == False:
         print("\nPlease choose a number\n")
     menuChoice = OptionsScripts.menuOptions(whatNext)
-    if menuChoice == 1:
+    #if menuChoice == 1:
         #something here
     if menuChoice == 2:
-        #open backpack function
-    if menuChoice == 3:
+        OptionsScripts.backpack()
+    #if menuChoice == 3:
         #something here
     
 
 puzzle1()
 
-def puzzle2 ():
-    listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door", "4. Quit the game"]
-    whatNext = ""
-    menuChoice = 0
-    print("\n")
-    for items in listOfMenuOptions:
-        print(items)
-    success = False
-    whatNext = input("\nWhat do you want to do next? Type the number to select an option\n")
-    if whatNext.isnumeric() == False:
-        print("\nPlease choose a number\n")
-    menuChoice = OptionsScripts.menuOptions(whatNext)
-    if menuChoice == 1:
-        #something here
-    if menuChoice == 2:
-        #open backpack function
-    if menuChoice == 3:
-        #something here
+# def puzzle2 ():
+#     listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door", "4. Quit the game"]
+#     whatNext = ""
+#     menuChoice = 0
+#     print("\n")
+#     for items in listOfMenuOptions:
+#         print(items)
+#     success = False
+#     whatNext = input("\nWhat do you want to do next? Type the number to select an option\n")
+#     if whatNext.isnumeric() == False:
+#         print("\nPlease choose a number\n")
+#     menuChoice = OptionsScripts.menuOptions(whatNext)
+#     if menuChoice == 1:
+#         #something here
+#     if menuChoice == 2:
+#         #open backpack function
+#     if menuChoice == 3:
+#         #something here
 
-def puzzle3 ():
-    listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door", "4. Quit the game"]
-    whatNext = ""
-    menuChoice = 0
-    print("\n")
-    for items in listOfMenuOptions:
-        print(items)
-    success = False
-    whatNext = input("\nWhat do you want to do next? Type the number to select an option\n")
-    if whatNext.isnumeric() == False:
-        print("\nPlease choose a number\n")
-    menuChoice = OptionsScripts.menuOptions(whatNext)
-    if menuChoice == 1:
-        #something here
-    if menuChoice == 2:
-        #open backpack function
-    if menuChoice == 3:
-        #something here
+# def puzzle3 ():
+#     listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door", "4. Quit the game"]
+#     whatNext = ""
+#     menuChoice = 0
+#     print("\n")
+#     for items in listOfMenuOptions:
+#         print(items)
+#     success = False
+#     whatNext = input("\nWhat do you want to do next? Type the number to select an option\n")
+#     if whatNext.isnumeric() == False:
+#         print("\nPlease choose a number\n")
+#     menuChoice = OptionsScripts.menuOptions(whatNext)
+#     if menuChoice == 1:
+#         #something here
+#     if menuChoice == 2:
+#         #open backpack function
+#     if menuChoice == 3:
+#         #something here
 
-def puzzle4 ():
-    listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door", "4. Quit the game"]
-    whatNext = ""
-    menuChoice = 0
-    print("\n")
-    for items in listOfMenuOptions:
-        print(items)
-    success = False
-    whatNext = input("\nWhat do you want to do next? Type the number to select an option\n")
-    if whatNext.isnumeric() == False:
-        print("\nPlease choose a number\n")
-    menuChoice = OptionsScripts.menuOptions(whatNext)
-    if menuChoice == 1:
-        #something here
-    if menuChoice == 2:
-        #open backpack function
-    if menuChoice == 3:
-        #something here
+# def puzzle4 ():
+#     listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door", "4. Quit the game"]
+#     whatNext = ""
+#     menuChoice = 0
+#     print("\n")
+#     for items in listOfMenuOptions:
+#         print(items)
+#     success = False
+#     whatNext = input("\nWhat do you want to do next? Type the number to select an option\n")
+#     if whatNext.isnumeric() == False:
+#         print("\nPlease choose a number\n")
+#     menuChoice = OptionsScripts.menuOptions(whatNext)
+#     if menuChoice == 1:
+#         #something here
+#     if menuChoice == 2:
+#         #open backpack function
+#     if menuChoice == 3:
+#         #something here
 
 class EscapeRoom:
     def __init__(self, name, spellStrength, puzzleCounter):
