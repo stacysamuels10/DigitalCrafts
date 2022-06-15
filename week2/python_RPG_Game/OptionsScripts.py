b9dfc00b9c4b62dbb2e15d7fba6aaffd126281e8
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-15 16:37:42 -0500

    backpack function

--- week2/python_RPG_Game/OptionsScripts.py
+++ week2/python_RPG_Game/OptionsScripts.py
@@ -23,11 +23,74 @@ def backpack (openBackpack):
     backpackItems = ["Your wand", "Hogwarts Student Handbook",\
      "Map of Library", "Beginner's Guide to Wand Motions" "Phone", \
      "Post it Note", "UV Blacklight Flashlight"]
-    if openBackpack == "1"
-    if openBackpack == "2"
-    if openBackpack == "3"
-    if openBackpack == "4"
-    if openBackpack == "5"
-    if openBackpack == "6"
-    if openBackpack == "7"
+    if openBackpack == "1":
+        backpackChoice ="your wand is cool"
+        return backpackChoice
+    if openBackpack == "2":
+        backpackChoice = """
+    1 Galleon equals 17 Sickles
+    1 Galleon equals 493 Knuts
+    1 Sickle equals 29 Knuts
+        """
+        return backpackChoice
+    if openBackpack == "3":
+        backpackChoice = """
+    200: Religion
+    300: Social Science
+    400: Literature and Poetry
+    600: Technology
+    900: History and Geography
+        """
+        return backpackChoice
+    if openBackpack == "4":
+        backpackChoice = """
+    A Beginner's Guide to Wand Motions"
+    This paper has pictures that show the different movements one makes with their wand to complete the different spells.
+    Leviosa: Move your wand in a circle clockwise then down
+    Expelliarmus: Move your wand to the side then down
+    Lumos: Sharp movement up
+    Nox: Sharp movement down
+    Stupefy: Move your wand diagonally then to the right
+        """
+        return backpackChoice
+    if openBackpack == "5":
+        backpackChoice = """
+Phone unlocked and shows a text exchange
 
+    Shannon: Do you know where the books are that we need for the first class?
+
+    Jill: What books?
+
+    Shannon: I need to find them in the library before everyone else checks them out
+
+    Jill: Okay, I am in the library now, but what books??
+
+    Shannon: Sizzling Scales: A Collection of Daring Dragon Poetry and Magical Maps and the Art of Not Getting Lost
+
+    Jill: That's all?
+
+    Shannon:  Yep- Sizzling Scales and Magical Maps.
+
+    Jill: Dragon Rhymes and maps.  Off to the shelves to find them for us. 
+
+    Shannon: Thanks!
+        """
+        return backpackChoice
+    if openBackpack == "6":
+        backpackChoice = """
+    1. Stupefy
+    2. Lumos
+    3. Leviosa
+"""
+        return backpackChoice
+    if openBackpack == "7":
+        backpackChoice = """
+    You see the book suddenly light up with stars next to 5 buildings
+
+    grinGotts wizarding bank
+    Ollivanders
+    twilifit and Tattings
+    eeYlops owl emporium
+    the leAky cauldron
+        """
+        return backpackChoice
--- week2/python_RPG_Game/puzzleMethods.py
+++ week2/python_RPG_Game/puzzleMethods.py
@@ -2,12 +2,12 @@ import OptionsScripts
 from time import sleep
 
 whatNext = ""
-backpackChoice = 0
+backpackChoice = ""
 menuChoice = 0
 
 def puzzle1 ():
-    print("""bookself, see map of library and brochure, 
-    you put both in your backpack\
+    print("""\nbookself, see map of library and brochure, 
+    you put both in your backpack
     maybe they will help later
     """)
     sleep(2)
@@ -17,20 +17,26 @@ def puzzle1 ():
         print(items)
     success = False
     whatNext = input("\nWhat do you want to do next? Type the number to select an option\n")
-    if whatNext.isnumeric() == False:
+    while whatNext.isnumeric() == False:
         print("\nPlease choose a number\n")
+        whatNext = input("\nWhat do you want to do next? Type the number to select an option\n")
     menuChoice = OptionsScripts.menuOptions(whatNext)
     #if menuChoice == 1:
         #something here
     if menuChoice == 2:
         backpackItems = ["1. Your wand", "2. Hogwarts Student Handbook",\
      "3. Map of Library", "4. Beginner's Guide to Wand Motions"]
+        print("\n")
         for item in backpackItems:
             print(item)
         openBackpack = input("\nWhat backpack item do you want?\n")
-        if openBackpack.isnumeric() == False:
-        print("\nPlease choose a number\n")
-        backpackChoice = OptionsScripts.menuOptions(openBackpack)
+        while openBackpack.isnumeric() == False:
+            print("\nPlease choose a number\n")
+            openBackpack = input("\nWhat backpack item do you want?\n")
+        backpackChoice = OptionsScripts.backpack(openBackpack)
+        while backpackChoice != "1" or backpackChoice != "2" or \
+        backpackChoice != "3" or backpackChoice != "4":
+            print(backpackChoice)
     #if menuChoice == 3:
         #something here
     
