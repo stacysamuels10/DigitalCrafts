dca73ffd402226cc86126591deed6fdd3113a75a
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-15 11:04:17 -0500

    Update introOutroScripts.py

--- week2/python_RPG_Game/introOutroScripts.py
+++ week2/python_RPG_Game/introOutroScripts.py
@@ -16,4 +16,24 @@ def introLetter():
     sleep(.7)
     print(f"Hence forward you will be known as {name}")
 
-introLetter()
+#introLetter()
+
+def introToGame():
+    print("You’ve accepted your invitation to Hogwarts and finally attend as a student.")
+    sleep(.7)
+    print("""
+    It's your first week and after 
+    staying up late in a new friends dorm, 
+    you realize they have played a prank on you 
+    and enchanted the room so you can't get out.
+    """)
+    sleep(.7)
+    print("""
+    You have your backpack but 
+    the rest of the room is new and different. 
+    You look at your watch.
+    You have an hour before your first class of Potions.
+    """)
+    sleep(.7)
+
+    introToGame()
