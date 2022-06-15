7ae6c8191c533d010b1ba461b2ab7caea35b7f47
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-15 12:12:47 -0500

    Update OptionsScripts.py

--- week2/python_RPG_Game/OptionsScripts.py
+++ week2/python_RPG_Game/OptionsScripts.py
@@ -1,10 +1,26 @@
 def menuOptions ():
-    listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door"]
+    listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door", "4. Quit the game"]
+    whatNext = ""
+    print("\n")
     for items in listOfMenuOptions:
-        print(items)
-    whatNext = input("What do you want to do next? Type the number to select an option\n")
-    if whatNext.isnumeric() == False:
-        print("Please choose a number") 
-
-
+            print(items)
+    while whatNext != "4":
+        whatNext = input("\nWhat do you want to do next? Type the number to select an option\n")
+        for items in listOfMenuOptions:
+            print(items)
+        if whatNext.isnumeric() == False:
+            print("\nPlease choose a number\n")
+            # for items in listOfMenuOptions:
+            #     print(items)
+        if whatNext == "1":
+            print("\nthis is where you will put the puzzle number method\n")
+            break
+        if whatNext == "2":
+            print("\nthis is where you will put the backpack class\n")
+            break
+        if whatNext =="3":
+            print("\nthis is where you will show the current spell level\n")
+            break
+    if whatNext == "4":
+        print("Thank you for playing!")
 menuOptions()
