70d0465cf934be0c0c25536808fd39bf5d7affbd
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-15 11:51:00 -0500

    Update OptionsScripts.py

---
+++ week2/python_RPG_Game/OptionsScripts.py
@@ -0,0 +1,4 @@
+def menuOptions ():
+    listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door"]
+
+    whatNext = int(input("What do you want to do next? Type the number to select an option\n")
