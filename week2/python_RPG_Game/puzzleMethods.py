e70aa90f95b14197279af2b8316005b777bece44
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-15 14:19:10 -0500

    starting over on pathways

---
+++ week2/python_RPG_Game/puzzleMethods.py
@@ -0,0 +1,5 @@
+class EscapeRoom:
+    def __init__(self, name, spellStrength, puzzleCounter):
+        self.name = name
+        self.spellStrength = spellStrength
+        self.puzzleCounter = puzzleCounter 
