d358f9ce6f33615e9e9ad67a24fe6b4be645bdb4
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-15 11:55:01 -0500

    forcing user to choose a number

--- week2/python_RPG_Game/OptionsScripts.py
+++ week2/python_RPG_Game/OptionsScripts.py
@@ -1,4 +1,10 @@
 def menuOptions ():
     listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door"]
+    for items in listOfMenuOptions:
+        print(items)
+    whatNext = input("What do you want to do next? Type the number to select an option\n")
+    if whatNext.isnumeric() == False:
+        print("Please choose a number") 
 
-    whatNext = int(input("What do you want to do next? Type the number to select an option\n")
+
+menuOptions()
