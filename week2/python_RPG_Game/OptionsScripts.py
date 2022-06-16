b1e0d04ceb26411fe4b01419b971f3217c4142ae
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-16 08:13:09 -0500

    starting puzzle 2

--- week2/python_RPG_Game/OptionsScripts.py
+++ week2/python_RPG_Game/OptionsScripts.py
@@ -12,7 +12,8 @@ def menuOptions (whatNext): #test and works
         menuChoice = 3
         return menuChoice
     if whatNext == "4":
-        print("\nThank you for playing! Bye\n")
+        menuChoice = 4
+        return menuChoice
 
 def invokeMenuOptions ():
     listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door", "4. Quit the game"]
@@ -54,6 +55,13 @@ def puzzle1():
         tryTheCode = input("Would you like to 1. Enter the code to sovle the puzzle or 2. View the menu? ")
     while tryTheCode == "1":
         puzzle1Code = input("Enter the code: ")
+        if puzzle1Code == "1300":
+            print("""Success! Cabinet cracks open and 
+            you hear a soft rumble through the floor, 
+            that must be the spell easing up
+            """)
+            success = True
+            return success
         while puzzle1Code != "1300":
             print("That is not the code. Please try again.\
         The spell power increases with each incorrect guess\n \
@@ -62,12 +70,40 @@ def puzzle1():
             if tryTheCode == "2":
                 return
             puzzle1Code = input("Enter the code: ")
-            if puzzle1Code == "1300":
-                print("""Success! Cabinet cracks open and 
+                    # increase spell counter
+
+def puzzle2():
+    success = False
+    puzzle2Code = ""
+    tryTheCode2 = ""
+    print("""
+        You walk up to the locked cabinet.
+        It has a number 1-9 keypad and 4 blanks
+        It says Library below it.
+        Maybe things in your backpack can be pieced together?        
+            """)
+    sleep(2)
+    tryTheCode = input("Would you like to 1. Enter the code to solve the puzzle or 2. View the menu? ")
+    if tryTheCode == "2":
+        return
+    while tryTheCode != "1" and tryTheCode != "2":
+        print("Please enter 1 or 2")
+        tryTheCode = input("Would you like to 1. Enter the code to sovle the puzzle or 2. View the menu? ")
+    while tryTheCode == "1":
+        puzzle1Code = input("Enter the code: ")
+        if puzzle1Code == "1300":
+            print("""Success! Cabinet cracks open and 
             you hear a soft rumble through the floor, 
             that must be the spell easing up
             """)
-                success = True
+            success = True
+            return success
+        while puzzle1Code != "1300":
+            print("That is not the code. Please try again.\
+        The spell power increases with each incorrect guess\n \
+        It should be a 4 digit number")
+            tryTheCode = input("Would you like to 1. Enter the code to sovle the puzzle or 2. View the menu? ")
+            if tryTheCode == "2":
                 return
+            puzzle1Code = input("Enter the code: ")
                     # increase spell counter
-
--- week2/python_RPG_Game/puzzleMethods.py
+++ week2/python_RPG_Game/puzzleMethods.py
@@ -5,10 +5,10 @@ from time import sleep
 whatNext = ""
 openBackpack = ""
 menuChoice = 0
+success = False
 
 
 def story1 ():
-    success = False
     puzzle1Code = ""
     menuChoice = 0
     whatNext = OptionsScripts.invokeMenuOptions()
@@ -22,32 +22,32 @@ def story1 ():
             backpackScripts.backpack1and2()
             whatNext = OptionsScripts.invokeMenuOptions()
             menuChoice = OptionsScripts.menuOptions(whatNext)
-
-
-    #if menuChoice == 3:
-        #something here
-    
+        if menuChoice == 3:
+            print("This is where score will go")
+        if menuChoice == 4:
+            print("okay bye!")
+            break
 
 story1()
-
-# def puzzle2 ():
-#     listOfMenuOptions = ["1. Look around the room", "2. Open my backpack", "3. Try the door", "4. Quit the game"]
-#     whatNext = ""
-#     menuChoice = 0
-#     print("\n")
-#     for items in listOfMenuOptions:
-#         print(items)
-#     success = False
-#     whatNext = input("\nWhat do you want to do next? Type the number to select an option\n")
-#     if whatNext.isnumeric() == False:
-#         print("\nPlease choose a number\n")
-#     menuChoice = OptionsScripts.menuOptions(whatNext)
-#     if menuChoice == 1:
-#         #something here
-#     if menuChoice == 2:
-    #     backpackfunction1and2
-#     if menuChoice == 3:
-#         #something here
+def story2 ():
+    puzzle2Code = ""
+    menuChoice = 0
+    whatNext = OptionsScripts.invokeMenuOptions()
+    menuChoice = OptionsScripts.menuOptions(whatNext)
+    while menuChoice != 0:
+        if menuChoice == 1:
+            OptionsScripts.puzzle2()
+            whatNext = OptionsScripts.invokeMenuOptions()
+            menuChoice = OptionsScripts.menuOptions(whatNext)
+        if menuChoice == 2:
+            backpackScripts.backpack1and2()
+            whatNext = OptionsScripts.invokeMenuOptions()
+            menuChoice = OptionsScripts.menuOptions(whatNext)
+        if menuChoice == 3:
+            print("This is where score will go")
+        if menuChoice == 4:
+            print("okay bye!")
+            break
 
 #def cutSceneTwo():
 
