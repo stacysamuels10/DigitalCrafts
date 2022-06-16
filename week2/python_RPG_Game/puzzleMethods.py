1538ac9a01536d0b803a76c87e01d2dd3c1c84f6
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-15 21:03:44 -0500

    Update puzzleMethods.py

--- week2/python_RPG_Game/puzzleMethods.py
+++ week2/python_RPG_Game/puzzleMethods.py
@@ -51,23 +51,33 @@ menuChoice = 0
 
 def puzzle1 ():
     success = False
+    puzzle1Code = ""
     whatNext = OptionsScripts.invokeMenuOptions()
     menuChoice = OptionsScripts.menuOptions(whatNext)
-    if menuChoice == 1:
+    while menuChoice == 1:
         print("""
     You walk up to the locked cabinet.
     It has a number 1-9 keypad and 4 blanks
     Maybe things in your backpack can be pieced together?        
         """)
         sleep(2)
-        puzzle1Code = int(input("Enter the code: "))
-        while puzzle1Code.isnumeric() == False:
-            print("\nPlease choose a number\n")
-            puzzle1Code = int(input("Enter the code: "))
-        while puzzle1Code != 1300:
-            print("That is not the code. Please try again. The spell power increases with each incorrect guess")
-            # increase spell counter
-    if menuChoice == 2:
+        tryTheCode = ""
+        tryTheCode = input("Would you like to 1. Enter the code to sovle the puzzle or 2. View the menu? ")
+        while tryTheCode != "1" and tryTheCode != "2":
+            print("Please enter 1 or 2")
+            tryTheCode = input("Would you like to 1. Enter the code to sovle the puzzle or 2. View the menu? ")
+        if tryTheCode == "1":
+            puzzle1Code = input("Enter the code: ")
+            while puzzle1Code.isnumeric() == False:
+                print("\nPlease choose a number\n")
+                puzzle1Code = int(input("Enter the code: "))
+            while puzzle1Code != "1300":
+                print("That is not the code. Please try again. The spell power increases with each incorrect guess")
+                # increase spell counter
+        else:
+            whatNext = OptionsScripts.invokeMenuOptions()
+            menuChoice = OptionsScripts.menuOptions(whatNext)
+    while menuChoice == 2:
         backpackItems = ["1. Your wand", "2. Hogwarts Student Handbook",\
      "3. Map of Library", "4. Beginner's Guide to Wand Motions", "5. Phone"]
         print("\n")
@@ -75,10 +85,6 @@ def puzzle1 ():
             print(item)
         openBackpack = input("\nWhat backpack item do you want?\n")
         backpackChoice = OptionsScripts.backpack(openBackpack)
-        if openBackpack == "1" or openBackpack == "2" or \
-        openBackpack == "3" or openBackpack == "4" or openBackpack == "5":
-            backpackChoice = OptionsScripts.backpack(openBackpack)
-            print(backpackChoice)
         while openBackpack.isnumeric() == False:
             for item in backpackItems:
                 print(item)
@@ -90,6 +96,15 @@ def puzzle1 ():
             print("Please choose a number between 1 and 5")
             openBackpack = input("\nWhat backpack item do you want?\n")
         backpackChoice = OptionsScripts.backpack(openBackpack)
+        while openBackpack == "1" or openBackpack == "2" or \
+        openBackpack == "3" or openBackpack == "4" or openBackpack == "5":
+            backpackChoice = OptionsScripts.backpack(openBackpack)
+            print(backpackChoice)
+            break
+        break
+    whatNext = OptionsScripts.invokeMenuOptions()
+    menuChoice = OptionsScripts.menuOptions(whatNext)
+
     #if menuChoice == 3:
         #something here
     
