9f8bf3fb2f845650003fc246f48c51f0f568d185
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-21 19:58:56 -0500

    homework

--- week3/day1/90sWebsiteHomework/main.js
+++ week3/day1/90sWebsiteHomework/main.js
@@ -1,9 +1,8 @@
 function changeImg() {
   var image = document.getElementById("systems");
   if (
-    image.src.match(
-      "http://taco.com/assets/images/autogen/Systems___and_Networks_Ntaco2big.gif"
-    )
+    image.src ==
+    "http://taco.com/assets/images/autogen/Systems___and_Networks_Ntaco2big.gif"
   ) {
     image.src =
       "http://taco.com/assets/images/autogen/Systems___and_Networks_Htaco2biga.gif";
