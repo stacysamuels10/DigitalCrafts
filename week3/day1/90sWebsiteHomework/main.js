65cea1ba62eadf5e79dc7854b985af2258184b79
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-21 20:36:32 -0500

    homework

--- week3/day1/90sWebsiteHomework/main.js
+++ week3/day1/90sWebsiteHomework/main.js
@@ -11,3 +11,69 @@ function changeImgSystems() {
       "http://taco.com/assets/images/autogen/Systems___and_Networks_Ntaco2big.gif";
   }
 }
+
+function changeImgHosting() {
+  var image = document.getElementById("hosting");
+  if (
+    image.src ==
+    "http://taco.com/assets/images/autogen/Web___Hosting_Ntaco2big.gif"
+  ) {
+    image.src =
+      "http://taco.com/assets/images/autogen/Web___Hosting_Htaco2biga.gif";
+  } else {
+    image.src =
+      "http://taco.com/assets/images/autogen/Web___Hosting_Ntaco2big.gif";
+  }
+}
+function changeImgCustom() {
+  var image = document.getElementById("custom");
+  if (
+    image.src ==
+    "http://taco.com/assets/images/autogen/Custom__PCs_Ntaco2big.gif"
+  ) {
+    image.src =
+      "http://taco.com/assets/images/autogen/Custom__PCs_Htaco2biga.gif";
+  } else {
+    image.src =
+      "http://taco.com/assets/images/autogen/Custom__PCs_Ntaco2big.gif";
+  }
+}
+function changeImgEquipment() {
+  var image = document.getElementById("equipment");
+  if (
+    image.src ==
+    "http://taco.com/assets/images/autogen/Systems___and_Networks_Ntaco2big.gif"
+  ) {
+    image.src =
+      "http://taco.com/assets/images/autogen/Systems___and_Networks_Htaco2biga.gif";
+  } else {
+    image.src =
+      "http://taco.com/assets/images/autogen/Systems___and_Networks_Ntaco2big.gif";
+  }
+}
+function changeImgWhy() {
+  var image = document.getElementById("why");
+  if (
+    image.src ==
+    "http://taco.com/assets/images/autogen/Systems___and_Networks_Ntaco2big.gif"
+  ) {
+    image.src =
+      "http://taco.com/assets/images/autogen/Systems___and_Networks_Htaco2biga.gif";
+  } else {
+    image.src =
+      "http://taco.com/assets/images/autogen/Systems___and_Networks_Ntaco2big.gif";
+  }
+}
+function changeImgPumps() {
+  var image = document.getElementById("pumps");
+  if (
+    image.src ==
+    "http://taco.com/assets/images/autogen/Systems___and_Networks_Ntaco2big.gif"
+  ) {
+    image.src =
+      "http://taco.com/assets/images/autogen/Systems___and_Networks_Htaco2biga.gif";
+  } else {
+    image.src =
+      "http://taco.com/assets/images/autogen/Systems___and_Networks_Ntaco2big.gif";
+  }
+}
