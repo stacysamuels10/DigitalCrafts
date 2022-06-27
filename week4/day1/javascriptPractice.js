cf63eb1dd80c97eef444629f88463ddb0daaa441
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-27 09:48:51 -0500

    friday and monday morning commits

---
+++ week4/day1/javascriptPractice.js
@@ -0,0 +1,14 @@
+// print in python
+console.log("hello world");
+let object = {
+  name: "Joe",
+  height: "5:8",
+  age: "42",
+  weight: "55kg",
+};
+const formerTas = ["rayleigh", "christian", "randy", "Q"];
+//console.table(array);
+
+//how to define variables in js
+// let, var, const
+//want to use let or const and not var
