31dc10c924f78cb31278ce450cc5bc73a7ae127c
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-27 10:37:42 -0500

    Update jsFunctions.js

--- week4/day1/jsFunctions.js
+++ week4/day1/jsFunctions.js
@@ -8,10 +8,21 @@ count = 2;
 count = 3;
 
 //use keyword function to define a function (the old way)
-function incrementNumber(number) {
-  return number++;
+function incrementNumber(number = 0) {
+  let total = number + 1;
+  return total;
   //function body
   //dont have to return unless you want to
 }
+//if you get NaN, you need to write code that can handle a number
+//false-y values in js "" '' ``, string length of zero, null, undefined, NaN, document.all, false
+console.log(incrementNumber(12));
 
-incrementNumber();
+//ES5 way of writing a function
+//arrow function
+//defining a function as a variable
+//make const a const variable, they should not change
+const arrowIncrement = (number) => {
+  let total = number + 1;
+  return total;
+};
