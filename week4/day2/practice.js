414ea7b52862db7024f33ee3ec58bf38a2d37da5
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-28 11:27:42 -0500

    updates

--- week4/day2/practice.js
+++ week4/day2/practice.js
@@ -2,9 +2,6 @@ console.log(document);
 
 //document.
 //getElementById
-const button1 = parseInt(document.getElementById("button1").innerText);
-const button2 = parseInt(document.getElementById("button2").innerText);
-const calculate = document.getElementById("calculate");
 //making a button that has a function
 //listener is more encompassing
 //addition.addEventListener("click", (howdy) => console.log(howdy));
@@ -13,16 +10,47 @@ const calculate = document.getElementById("calculate");
 // addition.onclick = () => console.log("I was clicked") event
 //addition.onclick = () => console.log("I was clicked");
 
-const addition = document.getElementById("addition");
+const numbersToCalc = [];
+let answer = "";
 
 const calculate = () => {};
 
 const sum = (total) => {
   console.log("this is the sum", total);
 };
-addition.onclick = sum;
-calculate.onclick = calculate;
-const buttons = document.getElementsByTagName("button");
-for (const button of buttons) {
-  console.log(button.innerText);
-}
+
+const getButtonValue = (buttonInnerText) => {
+  if (buttonInnerText.id === "addition") {
+    let mathSign = buttonInnerText.innerText;
+    numbersToCalc.push(mathSign);
+  } else {
+    const innerTextToInt = parseInt(buttonInnerText.innerText);
+    numbersToCalc.push(innerTextToInt);
+  }
+
+  console.log(numbersToCalc);
+};
+
+const doMaff = () => {
+  const firstVal = numbersToCalc[0];
+  const mathSign = numbersToCalc[1];
+  const secondVal = numbersToCalc[2];
+  if (mathSign === "+") {
+    console.log(firstVal + secondVal);
+    answer = firstVal + secondVal;
+    input.value = answer;
+  }
+};
+const input = document.getElementById("textbox");
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const calc = document.getElementById("calculate");
+const addition = document.getElementById("addition");
+
+calc.onclick = doMaff;
+button1.onclick = (e) => getButtonValue(e.target);
+button2.onclick = (e) => getButtonValue(e.target);
+addition.onclick = (e) => getButtonValue(e.target);
+const bigAnswer = document.createElement("h1");
+bigAnswer.innerText = "Cowboy";
+container.append(bigAnswer);
