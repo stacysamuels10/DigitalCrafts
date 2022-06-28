da71543cc3ec6310d63e74184cb620319111c2ce
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-28 10:48:12 -0500

    morning lecture

--- week4/day2/practice.js
+++ week4/day2/practice.js
@@ -4,9 +4,24 @@ console.log(document);
 //getElementById
 const button1 = parseInt(document.getElementById("button1").innerText);
 const button2 = parseInt(document.getElementById("button2").innerText);
+const calculate = document.getElementById("calculate");
+//making a button that has a function
+//listener is more encompassing
+//addition.addEventListener("click", (howdy) => console.log(howdy));
+//addEventListener("click", (e) => console.log(e)); almost everything
+// the element
+// addition.onclick = () => console.log("I was clicked") event
+//addition.onclick = () => console.log("I was clicked");
 
 const addition = document.getElementById("addition");
 
+const calculate = () => {};
+
+const sum = (total) => {
+  console.log("this is the sum", total);
+};
+addition.onclick = sum;
+calculate.onclick = calculate;
 const buttons = document.getElementsByTagName("button");
 for (const button of buttons) {
   console.log(button.innerText);
