435858bd59f7a01479a79e5f374dc01f59b7c9bc
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-06-29 11:43:09 -0500

    morning lecture

--- week4/day3/animal.js
+++ week4/day3/animal.js
@@ -1,2 +1,26 @@
 const dropdown = document.getElementById("animal-farm");
-console.log(dropdown.children[0].value);
+const animalNoiseContainer = document.querySelector(".animal-noise");
+const animalChoice = (emoji) => {
+  animalNoiseContainer.innerHTML = null;
+  //figure out how to create elements
+  const animalNoise = document.createElement("p");
+  //figure out how to attach those elements to the web pages
+  if (emoji === "🐷") {
+    console.log("oink oink");
+    animalNoise.innerText = "oink oink";
+  } else if (emoji === "🐱") {
+    console.log("meow");
+    animalNoise.innerText = "meow";
+  } else if (emoji === "🐶") {
+    animalNoise.innerText = "woof";
+    console.log("woof");
+  } else {
+    console.log("cluck cluck");
+    animalNoise.innerText = "cluck cluck";
+  }
+  animalNoiseContainer.append(animalNoise);
+};
+dropdown.onchange = (event) => animalChoice(event.target.value);
+
+//console.log(dropdown.children[0].value);
+// const pig = dropdown.children[0].value;
