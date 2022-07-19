a8b56da14957b344c2c502afb26d635cbdc16e51
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-07-19 14:01:52 -0500

    commit for afternoon practice

--- week7/day2/afternoon/index.js
+++ week7/day2/afternoon/index.js
@@ -6,6 +6,17 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.API_KEY);
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-app.post("/", (req, res) => {});
+app.use(express.json());
+
+app.post("/create_student", async (req, res) => {
+  console.log(req.body);
+  const { data, error } = await supabase.from("web-06-22").insert([req.body]);
+  if (data) {
+    res.send(data);
+  }
+  if (error) {
+    res.send(error);
+  }
+});
 
 app.listen(PORT, console.log(`listening on port ${PORT}`));
