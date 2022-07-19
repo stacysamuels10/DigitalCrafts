2c2ceb9683131c8aed5569bba9e84a23fb3bb912
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-07-19 13:30:23 -0500

    Update index.js

--- week7/day2/afternoon/index.js
+++ week7/day2/afternoon/index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
 const express = require("express");
+const { createClient } = require("@supabase/supabase-js");
+const supabase = createClient(process.env.SUPABASE_URL, process.env.API_KEY);
 const app = express();
-const PORT = process.envPORT || 3030;
+const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, console.log(`listening on port ${PORT}`));
