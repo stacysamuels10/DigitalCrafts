e73493528c03ef3175a9371f9f26822c8b570ea5
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-07-19 13:33:26 -0500

    Update index.js

--- week7/day2/afternoon/index.js
+++ week7/day2/afternoon/index.js
@@ -6,4 +6,6 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.API_KEY);
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+app.post("/", (req, res) => {});
+
 app.listen(PORT, console.log(`listening on port ${PORT}`));
