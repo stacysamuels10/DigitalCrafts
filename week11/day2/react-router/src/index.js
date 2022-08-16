e017d3573deb56193650e9a1ce21a305e850ba2d
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-08-16 09:51:02 -0500

    nav bar addition

--- week11/day2/react-router/src/index.js
+++ week11/day2/react-router/src/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import About from "./components/About";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <Navbar></Navbar>
     <Routes>
       <Route path="/" element={<App />}></Route>
+      <Route path="/about" element={<About />}></Route>
     </Routes>
   </BrowserRouter>
 );
